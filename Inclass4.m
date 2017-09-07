935db3df8b9775fa45302ddeb80b65d6daf18f41
Aryeh <warmflasha@gmail.com>
2017-09-07 11:10:59 -0500

    wrote Inclass4

---
+++ Inclass4.m
@@ -0,0 +1,11 @@
+%Inclass assignment 4. Due at the start of class on 9/12/17
+
+%1. (a) Write code that makes a file with the words "Random numbers 1" on its
+%own line, then 10 lines with 13 random numbers each, then "Random numbers
+%2" on its own line, then 9 lines with 7 random numbers each. (b) Write code
+%that reads only the first line of random numbers in the file and stores
+%them as numbers in an array. 
+
+% 2. Write a function that takes an array as input and returns a logical
+% variable which is true if the sum of the numbers in the array is greater
+% than or equal to 10 and false if it is less than 10. 
